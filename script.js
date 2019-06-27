ff6f9b918524226bfee8776409e514b08d2bb8e5
gravisanima <49438361+gravisanima@users.noreply.github.com>
2019-06-27 17:29:02 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ let scrollToElement1 = () =>{
         behavior: "smooth"
     });
 }
+
+let scrollToTop1 = () =>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
+ymaps.ready(init);
+function init(){
+  var myMap = new ymaps.Map("map", {
+    center: [42.837520, 74.623187],
+    zoom: 17
+  });
+  var myGeoObject = new ymaps.GeoObject({
+    geometry: {
+        type: "Point",
+        coordinates: [42.837520, 74.623187]
+      }
+  });
+  myMap.geoObjects.add(myGeoObject);
+}
